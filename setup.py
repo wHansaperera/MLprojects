a9d842d8d99b1b68fd7f7bd3ccb544a814d9904a
Hansaka Perera <112180549+wHansaperera@users.noreply.github.com>
2025-02-06 13:50:13 +0530

    second day

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages,setup
+
+
+setup(
+
+
+    name = "MLproject",
+    version = "0.0.1",
+    author = "Hansaka Perera",
+    author_email = "whansakaperera@gmail.com",
+    packages = find_packages(),
+    install_requires=['numpy','pandas','seaborn']
+
+
+
+)
