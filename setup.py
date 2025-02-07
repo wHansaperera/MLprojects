89da9fe1168df6e12f91a6239d25c872eab670eb
Hansaka Perera <112180549+wHansaperera@users.noreply.github.com>
2025-02-07 21:40:49 +0530

    second day

--- setup.py
+++ setup.py
@@ -1,15 +1,27 @@
 from setuptools import find_packages,setup
+from typing import list
 
+HYPEN_E_DOT = '-e .'
+def get_requirements(file_path:str) -> list[str]:
 
+    '''  
+        This function will returns the list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as file.obj:
+        requirements = file.obj.readlines()
+        requirements = [req.replace("\n","") for req in requirements ]
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
 setup(
-
-
     name = "MLproject",
     version = "0.0.1",
     author = "Hansaka Perera",
     author_email = "whansakaperera@gmail.com",
     packages = find_packages(),
-    install_requires=['numpy','pandas','seaborn']
+    install_requires=get_requirements('requirements.txt')
 
 
 
